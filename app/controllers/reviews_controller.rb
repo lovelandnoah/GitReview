4dfe648f2b5db20cce67e53980526e42f4c63f2c
Sam Hammer <sam@michaelrobertson.com>
2016-01-16 13:52:08 -0700

    controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
+
     @github = Github.new client_id: ENV["github_client_id"], client_secret:  ENV["github_client_secret"]
     @github1 = ""
     @github2 = ""
@@ -30,6 +32,20 @@ class ReviewsController < ApplicationController
   end
 
   def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to reviews_path(@review.user_id)
+    else
+      render :new
+    end
   end
 
   # def self.search(search)
@@ -44,6 +60,17 @@ class ReviewsController < ApplicationController
   def edit
   end
 
-  def new
+  def update
+    if @review.update(review_params)
+      redirect_to review_path
+    else
+      render :new
+    end
+  end
+
+  private
+    
+  def review_params
+    params.require(:review).permit(:author, :title, :content, :star )
   end
 end
